2c0715b198377368ae1e7a2411c4ccec116d527d
anguenth <anguenther@gmx.de>
2019-05-23 14:54:18 +0200

    lotto() statt lottoziehung() NEU: eurojackpot()

--- startfile.R
+++ startfile.R
@@ -6,24 +6,32 @@
 ## ziehung()
 
 ## von remote sourcen per devtools::source_url(<link zur file-raw-version>)
-## ABER "private" Repositories verwenden einen token.
-##       Diesen zuvor in github nachschlagen 
-##       (Datei in der Ansicht "raw" anklicken, dann siehe website-adressleiste)
+## ABER für "private" Repositories in GitLab funktioniert es nicht, da keine Möglichkeit, sich zu authentifizieren.
 # library(devtools)
-# gitHub_code <- "https://raw.githubusercontent.com/anguenth/lotto/master/startfile.R?token=ADC234JIJCKS7CXBALKO4L244PETO" 
-# gitHub_code <- "https://raw.githubusercontent.com/anguenth/lottoziehung/master/startfile.R"
-# source_url(gitHub_code)
+# gitLab_code <- "https://gitlab.com/anguenther/lottoziehung/raw/master/lottoziehung.R"
+# source_url(gitLab_code)
 
 
-lottoziehung <- function(){
+## 6 aus 49 plus Superzahl
+lotto <- function(){
     
-    ## 6 aus 49
-    hz <- sort(sample(1:49, 6))                ## dflt: replace=FALSE in sample(1:49, 6, replace=FALSE), d.h. ohne Zurücklegen
-    ## sort(floor(runif(6, min=1, max=50))) ist immer mit Zurücklegen, daher ungeeignet 
-    ## Superzahl
+    
+    hz <- sort(sample(1:49, 6))                ## dflt: replace=FALSE in sample(1:49, 6, replace=FALSE), d.h. ohne Zurücklegen ## sort(floor(runif(6, min=1, max=50))) ist immer mit Zurücklegen, daher ungeeignet 
     sz <- sample(0:9, 1)   
     
     result <- paste0("6 aus 49 Tipp: ",Reduce("paste",hz)," ... SZ ",Reduce("paste",sz)) 
+    
+    return(result)
+}
+
+
+## 5 aus 50 plus 2 Eurozahlen
+eurojackpot <- function(){
+    
+    hz <- sort(sample(1:50, 5))                ## dflt: replace=FALSE in sample(1:49, 6, replace=FALSE), d.h. ohne Zurücklegen
+    ez <- sample(1:10, 2)   
+    
+    result <- paste0("5 aus 50 Tipp: ",Reduce("paste",hz)," ... EZ ",Reduce("paste",ez)) 
     
     return(result)
 }
